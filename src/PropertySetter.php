23be559ad751fb4577eff42408fffb5f88ece271
Sebastian Szczepanski <sebastian@ably.pl>
2019-02-07 00:46:11 +0100

    add property setter

---
+++ src/PropertySetter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace LaPress\Support;
+
+/**
+ * @author    Sebastian Szczepański
+ * @copyright ably
+ */
+class PropertySetter
+{
+    /**
+     * @param object $object
+     * @param array $data
+     * @return object
+     */
+    public static function set($object, array $data = [])
+    {
+        $instance = new static();
+
+        foreach ($data as $key => $value) {
+            $methodName = $instance->getMethodName($key);
+
+            if (!method_exists($object, $methodName)) {
+                continue;
+            }
+
+            $object->{$methodName}($value);
+        }
+
+        return $object;
+    }
+
+    /**
+     * @param $key
+     * @return string
+     */
+    public function getMethodName($key): string
+    {
+        return sprintf('set%s', camel_case($key));
+    }
+}
